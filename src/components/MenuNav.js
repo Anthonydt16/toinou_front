48e742442f7222f49aa15af6c858ab196faacb4c
Anthonydt16 <@email>
2022-09-24 11:48:16 +0200

    menu déroulant ok

--- src/components/MenuNav.js
+++ src/components/MenuNav.js
@@ -6,10 +6,26 @@ const MenuNav = () => {
             //    un menu de navigation
             <nav className="menu">
                 <div className="container-menu">
+                                   
                     <NavLink to="/" className={(nav) => nav.isActive ? "nav-active" : ""}><li>Accueil</li></NavLink>
                     <NavLink to="/programme" className={(nav) => nav.isActive ? "nav-active" : ""}><li>Programmation</li></NavLink>
                     <NavLink to="/parcours" className={(nav) => nav.isActive ? "nav-active" : ""}><li>Parcours</li></NavLink>
                     <NavLink to="/contact" className={(nav) => nav.isActive ? "nav-active" : ""}><li>Contact</li></NavLink>
+                </div>
+                
+                <div className={"menuMobile"}>
+                    <span className="burger-icon">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                      </span>
+                    <div className={"childMenu"}>
+                         <NavLink to="/" className={(nav) => nav.isActive ? "nav-active" : ""}><li>Accueil</li></NavLink>
+                         <NavLink to="/programme" className={(nav) => nav.isActive ? "nav-active" : ""}><li>Programmation</li></NavLink>
+                         <NavLink to="/parcours" className={(nav) => nav.isActive ? "nav-active" : ""}><li>Parcours</li></NavLink>
+                         <NavLink to="/contact" className={(nav) => nav.isActive ? "nav-active" : ""}><li>Contact</li></NavLink>
+                    </div>
+
                 </div>
                 <div className={"container-logo"}>
                     <img src={logo}  alt={"logo"}/>
